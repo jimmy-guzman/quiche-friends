b0ec7de0b11bb1225dbb8843c14e764e4ae2b6cd
kvrmd <kvrmd25@gmail.com>
2018-11-04 15:34:23 -0800

    SS

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,57 +1,11 @@
 import React, { Component } from 'react'
-import LoginCamera from '../components/LoginCamera'
-import SignupCamera from '../components/SignupCamera'
-import * as api from '../utils/api'
+import LoginSignup from '../components/LoginSignup'
+
 class LandingPage extends Component {
-  state = {}
-
-  toggleLogin = () => {
-    this.setState(() => {
-      return { showLogin: !this.state.showLogin }
-    })
-  }
-
-  toggleSignup = () => {
-    this.setState(() => {
-      return { showSignup: !this.state.showSignup }
-    })
-  }
   render() {
     return (
       <div>
-        <button onClick={this.toggleLogin}>Log In</button>
-        {this.state.showLogin && (
-          <LoginCamera
-            onMatchFound={conceptID => {
-              this.setState({ error: false })
-              api.userLogin(conceptID).then(response => {
-                localStorage.setItem('user', response)
-                this.props.history.push('/home')
-              })
-            }}
-            onMatchNotFound={() => {
-              this.setState({ error: 'Your face was not found!' })
-            }}
-          />
-        )}
-        {this.state.error && <h1>{this.state.error}</h1>}
-
-        <button onClick={this.toggleSignup}>Sign Up</button>
-        {this.state.showSignup && (
-          <SignupCamera
-            onSignupSuccess={state => {
-              api
-                .userCreate({
-                  conceptID: state.conceptID,
-                  profileImage: state.profileImage
-                })
-                .then(response => {
-                  localStorage.setItem('user', response)
-                  this.props.history.push('/home')
-                })
-            }}
-          />
-        )}
+        <LoginSignup />
       </div>
     )
   }
