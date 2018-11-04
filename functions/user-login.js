880bb14d95b3e8dfaa32a36b987c133deecd4d74
Ian Serlin <ian@ianserlin.com>
2018-11-04 14:21:09 -0800

    switch user login to be by uuid

--- functions/user-login.js
+++ functions/user-login.js
@@ -7,17 +7,22 @@ const client = new faunadb.Client({
 
 exports.handler = async (event, context, callback) => {
   const query = JSON.parse(event.body);
-  console.log(`proposition-read' invoked. Read id: ${query.userId}`)
+  console.log('Function `user-login` invoked')
   try {
-    const response = client.query(q.Get(q.Ref(`classes/users/${query.userId}`)));
+    const response = await client.query(q.Match(q.Index("user_by_uuid"), query.uuid));
+    // create new query out of comment refs. http://bit.ly/2LG3MLg
+    const usersForUUID = response.data.map(ref => q.Get(ref));
+    const users = await client.query(usersForUUID);
     return {
       statusCode: 200,
-      body: JSON.stringify(response)
+      body: JSON.stringify(users)
     };
-  } catch(err) {
+  } catch(error) {
+    console.log('error', error)
     return {
       statusCode: 400,
       body: JSON.stringify(error)
     };
   }
+
 }
