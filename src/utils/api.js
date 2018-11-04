f9c784507a9eb3789641762e606c67dc7db26355
Ian Serlin <ian@ianserlin.com>
2018-11-04 11:12:26 -0800

    add geolocation information to each create command

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,18 @@
 /* Api methods to call /functions */
 
+let currentLocation;
+const IPSTACK_API_KEY = '7e9a9d0b23a735947e564fd07d356803'
+
+async function getLocation() {
+  const response = await fetch(`http://api.ipstack.com/check?access_key=${IPSTACK_API_KEY}&format=1`)
+  currentLocation = await response.json();
+}
+
+// save location in startup
+getLocation();
+
 export const createProposition = (data) => {
+  data.geolocation = currentLocation;
   return fetch('/.netlify/functions/proposition-create', {
     body: JSON.stringify(data),
     method: 'POST'
@@ -10,6 +22,7 @@ export const createProposition = (data) => {
 }
 
 export const createComment = (data) => {
+  data.geolocation = currentLocation;
   return fetch('/.netlify/functions/comment-create', {
     body: JSON.stringify(data),
     method: 'POST'
@@ -19,6 +32,7 @@ export const createComment = (data) => {
 }
 
 export const createVote = (data) => {
+  data.geolocation = currentLocation;
   return fetch('/.netlify/functions/vote-create', {
     body: JSON.stringify(data),
     method: 'POST'
