b9312eb8a21445a1020c53083e9399bac44c577c
jimmy-guzman <jimmy.guzman@outlook.com>
2018-11-04 10:39:20 -0800

    refactor form

--- src/Pages/CreateProposition.js
+++ src/Pages/CreateProposition.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
 import { Form } from '../components/Form'
 class CreateProposition extends Component {
+  state = {
+    title: 'Title',
+    description: 'Description',
+    pollOptions: 'Poll Options',
+    country: 'Country',
+    state: 'State',
+    city: 'City'
+  }
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    })
+  }
   render() {
-    return <Form />
+    return <Form handleChange={this.handleChange} {...this.state} />
   }
 }
 
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 import { withStyles } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
@@ -22,76 +21,51 @@ const styles = theme => ({
   }
 })
 
-class TextFields extends React.Component {
-  state = {
-    title: 'Title',
-    description: 'Description',
-    pollOptions: 'Poll Options',
-    country: 'Country',
-    state: 'State',
-    city: 'City'
-  }
+const Form = ({ classes, handleChange, ...formValues }) => (
+  <form className={classes.container} noValidate autoComplete="off">
+    <TextField
+      label="Title"
+      className={classes.textField}
+      value={formValues.title}
+      onChange={handleChange('title')}
+      margin="normal"
+    />
+    <TextField
+      label="Description"
+      className={classes.textField}
+      value={formValues.description}
+      onChange={handleChange('description')}
+      margin="normal"
+    />
+    <TextField
+      label="Poll Options"
+      className={classes.textField}
+      value={formValues.pollOptions}
+      onChange={handleChange('pollOptions')}
+      margin="normal"
+    />
+    <TextField
+      label="Country"
+      className={classes.textField}
+      value={formValues.country}
+      onChange={handleChange('country')}
+      margin="normal"
+    />
+    <TextField
+      label="State"
+      className={classes.textField}
+      value={formValues.state}
+      onChange={handleChange('state')}
+      margin="normal"
+    />
+    <TextField
+      label="Title"
+      className={classes.textField}
+      value={formValues.city}
+      onChange={handleChange('city')}
+      margin="normal"
+    />
+  </form>
+)
 
-  handleChange = name => event => {
-    this.setState({
-      [name]: event.target.value
-    })
-  }
-
-  render() {
-    const { classes } = this.props
-
-    return (
-      <form className={classes.container} noValidate autoComplete="off">
-        <TextField
-          label="Title"
-          className={classes.textField}
-          value={this.state.title}
-          onChange={this.handleChange('title')}
-          margin="normal"
-        />
-        <TextField
-          label="Description"
-          className={classes.textField}
-          value={this.state.description}
-          onChange={this.handleChange('description')}
-          margin="normal"
-        />
-        <TextField
-          label="Poll Options"
-          className={classes.textField}
-          value={this.state.pollOptions}
-          onChange={this.handleChange('pollOptions')}
-          margin="normal"
-        />
-        <TextField
-          label="Country"
-          className={classes.textField}
-          value={this.state.country}
-          onChange={this.handleChange('country')}
-          margin="normal"
-        />
-        <TextField
-          label="State"
-          className={classes.textField}
-          value={this.state.state}
-          onChange={this.handleChange('state')}
-          margin="normal"
-        />
-        <TextField
-          label="Title"
-          className={classes.textField}
-          value={this.state.city}
-          onChange={this.handleChange('city')}
-          margin="normal"
-        />
-      </form>
-    )
-  }
-}
-
-TextFields.propTypes = {
-  classes: PropTypes.object.isRequired
-}
-
-export default withStyles(styles)(TextFields)
+export default withStyles(styles)(Form)
