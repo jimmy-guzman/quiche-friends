bfd7007f16933396b2c80a7ee16095ac24e5f544
Ian Serlin <ian@ianserlin.com>
2018-11-04 14:16:43 -0800

    WIP proposition

--- src/Pages/Proposition.js
+++ src/Pages/Proposition.js
@@ -1,9 +1,62 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+import { getProposition, getPropositionComments } from '../utils/api'
+
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+});
 
 class Proposition extends Component {
+  state = {
+    proposition: {
+      title: "Loading",
+      description: ""
+    },
+    comments: []
+  }
+
+  async componentDidMount() {
+    const { propositionId } = this.props;
+    const proposition = await getProposition(propositionId);
+    const comments = await getPropositionComments(propositionId);
+    this.setState({
+      proposition,
+      comments
+    });
+
+  }
+
   render() {
-    return <div>Proposition</div>
+    const { classes } = this.props;
+    const { proposition, comments } = this.state;
+    return (
+      <div>
+        <Paper className={classes.root} elevation={1}>
+          <Typography variant="headline" component="h3">
+            {proposition.title}
+          </Typography>
+          <Typography component="p">
+            {proposition.description}
+          </Typography>
+        </Paper>
+        <ul>
+          {comments.map(c => <li>{c.text}</li>)}
+        </ul>
+      </div>
+    );
   }
 }
 
-export default Proposition
+Proposition.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Proposition)
