1720de3d8f3623fe5614784305ca4601e376c576
jimmy-guzman <jimmy.guzman@outlook.com>
2018-11-03 18:12:23 -0700

    renamed create to createProposition

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-import { Home, Proposition, SignUp, LandingPage, Create } from './Pages'
+import { Home, Proposition, SignUp, LandingPage, CreateProposition } from './Pages'
 
 class Main extends Component {
   render() {
@@ -11,7 +11,7 @@ class Main extends Component {
           <Route exact path="/" component={LandingPage} />
           <Route path="/home" component={Home} />
           <Route path="/signup" component={SignUp} />
-          <Route path="/proposition/create" component={Create} />
+          <Route path="/proposition/create" component={CreateProposition} />
           <Route path="/proposition/:id" component={Proposition} />
         </Switch>
       </Fragment>
--- src/Pages/Create.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-class Create extends Component {
-  render() {
-    return <div>Create</div>
-  }
-}
-
-export default Create
