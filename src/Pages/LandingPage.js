f9b2c17a409ab9e1431ded4b0bb43f2c62fafc0b
kvrmd <kvrmd25@gmail.com>
2018-11-04 10:37:25 -0800

    LoginCam

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
+import LoginCamera from '../components/LoginCamera'
 
 class LandingPage extends Component {
+  state = {}
+
+  toggleLogin = () => {
+    this.setState(() => {
+      return { showLogin: !this.state.showLogin }
+    })
+  }
   render() {
-    return <div>LandingPage</div>
+    return (
+      <div>
+        <button onClick={this.toggleLogin}>Log In</button>
+        {this.state.showLogin && <LoginCamera />}
+      </div>
+    )
   }
 }
 
--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import Selfie from '../components/Selfie'
+import LoginCamera from '../components/LoginCamera'
 class SignUp extends Component {
   render() {
     return (
       <div>
         <h1>Signup</h1>
-        <Selfie />
+        <LoginCamera />
       </div>
     )
   }
