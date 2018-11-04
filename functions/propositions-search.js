991884eb432707aef4543d3cc5ce5f2d3fb84220
jimmy-guzman <jimmy.guzman@outlook.com>
2018-11-04 14:24:14 -0800

    fix propositions-search

--- functions/propositions-search.js
+++ functions/propositions-search.js
@@ -6,32 +6,31 @@ const client = new faunadb.Client({
 })
 
 exports.handler = async (event, context, callback) => {
-  const query = JSON.parse(event.body);
-  console.log('Function `proposition-comments` invoked')
+  const query = JSON.parse(event.body)
+  console.log('Function `proposition-search` invoked')
   try {
     const response = await client.query(
       q.Paginate(
         q.Union(
-          q.Match(q.Index("propositions_by_country"), query.country),
-          q.Match(q.Index("propositions_by_state"), query.state),
-          q.Match(q.Index("propositions_by_count"), query.county),
-          q.Match(q.Index("propositions_by_city"), query.city),
+          q.Match(q.Index('prop_by_country'), query.country),
+          q.Match(q.Index('prop_by_state'), query.state),
+          q.Match(q.Index('prop_by_country'), query.county),
+          q.Match(q.Index('prop_by_city'), query.city)
         )
       )
-    );
+    )
     // create new query out of comment refs. http://bit.ly/2LG3MLg
-    const propositionsQuery = response.data.map(ref => q.Get(ref));
-    const propositions = await client.query(propositionsQuery);
+    const propositionsQuery = response.data.map(ref => q.Get(ref))
+    const propositions = await client.query(propositionsQuery)
     return {
       statusCode: 200,
       body: JSON.stringify(propositions)
-    };
-  } catch(error) {
+    }
+  } catch (error) {
     console.log('error', error)
     return {
       statusCode: 400,
       body: JSON.stringify(error)
-    };
+    }
   }
-
 }
